efdcbc29f328ddce0f8e4316b3f64a3183d589f3
SlavaGr <Slavag76@yandex.ru>
2021-06-30 20:12:45 +0300

    Initial commit

---
+++ app/src/main/java/com/testlslavag76/basedatafirebase1/MainActivity.java
@@ -0,0 +1,51 @@
+package com.testlslavag76.basedatafirebase1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText edName, edSecondName, edEmail;
+    private DatabaseReference mDataBase;
+    private String USER_KEY = "User";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+    }
+
+    public void init() {
+        edName = findViewById(R.id.edName);
+        edSecondName = findViewById(R.id.edSecondName);
+        edEmail = findViewById(R.id.edEmail);
+        mDataBase = FirebaseDatabase.getInstance().getReference(USER_KEY);
+    }
+
+    public void onClickSave(View view) {
+
+        String id = mDataBase.getKey();
+        String name = edName.getText().toString();
+        String secname = edSecondName.getText().toString();
+        String email = edEmail.getText().toString();
+        User newUser = new User(id, name, secname, email);
+        if(!TextUtils.isEmpty(name) && !TextUtils.isEmpty(secname) && !TextUtils.isEmpty(email)) {
+            mDataBase.push().setValue(newUser);
+        }else{
+            Toast.makeText(this, "пустое поле", Toast.LENGTH_SHORT);
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/com/testlslavag76/basedatafirebase1/User.java
@@ -0,0 +1,15 @@
+package com.testlslavag76.basedatafirebase1;
+
+public class User {
+    public String id, name, secondName, email;
+
+    public User() {
+    }
+
+    public User(String id, String name, String secondName, String email) {
+        this.id = id;
+        this.name = name;
+        this.secondName = secondName;
+        this.email = email;
+    }
+}
